0b54292bb57881a4d871b745278eac7ffdc4f896
Neculache <cosmin.neculache@gmail.com>
2023-08-28 16:57:56 +0300

    Deleted duplicated initialization for the 'Librarian' class

--- classes/librarian.py
+++ classes/librarian.py
@@ -5,10 +5,7 @@ from .member import Member
 class Librarian(Person):
     def __init__(self, first_name, last_name, age, employment_date):
         super().__init__(first_name, last_name, age)
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-        self.emplyment_date = employment_date
+        self.employment_date = employment_date
 
     def register_member(self, first_name, last_name, age, member_id, email, library):
         new_member = Member(first_name, last_name, age, member_id, [], email, library)
@@ -17,12 +14,10 @@ class Librarian(Person):
 
     def check_disponibility(self, physical_book):
         if physical_book.status == "available":
-            print("The book " + physical_book.title + " written by "
-                  + physical_book.writer + " it's available.")
+            print(f"The book {physical_book.title} written by {physical_book.writer} it's available.")
             return True
         else:
-            print("The book " + physical_book.title + " written by "
-                  + physical_book.writer + " it's not available. ")
+            print(f"The book {physical_book.title} written by {physical_book.writer} it's not available.")
             return False
 
     def __repr__(self):
