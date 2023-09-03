b90a7e16f1c04ce8e36d20da5c5f8108eaaaff96
Neculache <cosmin.neculache@gmail.com>
2023-09-03 19:44:30 +0300

    Updated test cases for 'Book' class

--- tests/test_book.py
+++ tests/test_book.py
@@ -4,15 +4,16 @@ from LibraSim.classes.book import Book
 
 class TestBookClass(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.book = Book("Fahrenheit 451", "Ray Bradbury")
+
     def test_book_init(self):
-        book = Book("Fahrenheit 451", "Ray Bradbury")
-        self.assertEqual(book.title, "Fahrenheit 451")
-        self.assertEqual(book.writer, "Ray Bradbury")
+        self.assertEqual(self.book.title, "Fahrenheit 451")
+        self.assertEqual(self.book.writer, "Ray Bradbury")
 
     def test_book_repr(self):
-        book = Book("Fahrenheit 451", "Ray Bradbury")
         expected_repr = "Book(title='Fahrenheit 451', writer='Ray Bradbury')"
-        self.assertEqual(repr(book), expected_repr)
+        self.assertEqual(repr(self.book), expected_repr)
 
 
 if __name__ == '__main__':
