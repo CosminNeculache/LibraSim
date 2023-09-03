db0415aa38900adcb0fb47c7f5493e20d616b426
Neculache <cosmin.neculache@gmail.com>
2023-09-03 19:37:56 +0300

    Updated test cases for 'Person' class

--- tests/test_person.py
+++ tests/test_person.py
@@ -5,13 +5,14 @@ from LibraSim.classes.person import Person
 class TestPersonClass(unittest.TestCase):
 
     def test_person_init(self):
-        person = Person("John", "Doe", 30)
+        person = Person("John", "Doe", 30, "john@example.com")
         self.assertEqual(person.first_name, "John")
         self.assertEqual(person.last_name, "Doe")
         self.assertEqual(person.age, 30)
+        self.assertEqual(person.email, "john@example.com")
 
     def test_person_repr(self):
-        person = Person("John", "Doe", 30)
+        person = Person("John", "Doe", 30, "john@example.com")
         expected_repr = "Person(John, Doe, 30)"
         self.assertEqual(repr(person), expected_repr)
 
