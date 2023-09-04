bf892b19171f140753cc3d751077a339969e52f6
Neculache <cosmin.neculache@gmail.com>
2023-09-04 22:47:40 +0300

    Created error handling for 'send_email' method, created logger

--- classes/send_email.py
+++ classes/send_email.py
@@ -1,6 +1,7 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
+import logging
 
 
 class SendEmail:
@@ -10,18 +11,25 @@ class SendEmail:
         self.sender_email = sender_email
         self.password = password
 
-    def send_email(self, receiver_email, subject, body):
-        message = MIMEMultipart()
-        message["From"] = self.sender_email
-        message["To"] = receiver_email
-        message["Subject"] = subject
-        message.attach(MIMEText(body, "plain"))
+        self.logger = logging.getLogger("SendEmail")
+        self.logger.setLevel(logging.ERROR)
 
-        server = smtplib.SMTP(self.smtp_server, self.smtp_port)
-        server.starttls()
-        server.login(self.sender_email, self.password)
-        server.sendmail(self.sender_email, receiver_email, message.as_string())
-        server.quit()
+    def send_email(self, receiver_email, subject, body):
+        try:
+            message = MIMEMultipart()
+            message["From"] = self.sender_email
+            message["To"] = receiver_email
+            message["Subject"] = subject
+            message.attach(MIMEText(body, "plain"))
+
+            with smtplib.SMTP(self.smtp_server, self.smtp_port) as server:
+                server.starttls()
+                server.login(self.sender_email, self.password)
+                server.sendmail(self.sender_email, receiver_email, message.as_string())
+                self.logger.info(f"Email sent successfully to {receiver_email}")
+        except Exception as e:
+            self.logger.error(f"Error sending email to {receiver_email}: {str(e)}")
+            raise
 
     def __repr__(self):
         return f"Email('{self.smtp_server}', {self.smtp_port}, '{self.sender_email}')"
